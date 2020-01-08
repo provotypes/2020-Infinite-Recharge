04c9202018c74b4e40fb3b62127433bafda1b0b2
Dallin <rasmudal12@gmail.com>
2020-01-07 20:57:29 -0700

    Begun Working on Drivetrain Class

--- src/main/java/frc/robot/Drivetrain.java
+++ src/main/java/frc/robot/Drivetrain.java
@@ -1,15 +1,48 @@
 package frc.robot;
 
-public class Drivetrain {
+import edu.wpi.first.wpilibj.SpeedControllerGroup;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import frc.robot.easypath.EasyPathDrivetrain;
+
+public class Drivetrain extends DifferentialDrive implements EasyPathDrivetrain{
     
+    private static SpeedControllerGroup leftGroup;
+    private static SpeedControllerGroup rightGroup;
     private static Drivetrain instance;
 
-    private Drivetrain(){}
+    private Drivetrain(){
+        super(leftGroup, rightGroup);
+    }
     
     public static Drivetrain getInstance(){
         if(instance == null){
             instance = new Drivetrain();
         }
         return instance;
+    }
+
+    @Override
+    public void setLeftRightDriveSpeed(double left, double right) {
+        
+        tankDrive(left, right);
+    
+    }
+
+    @Override
+    public double getInchesTraveled() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public double getCurrentAngle() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public void resetEncodersAndGyro() {
+        // TODO Auto-generated method stub
+
     }
 }
