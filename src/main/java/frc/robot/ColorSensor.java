ccabcb1067906cc77f801c482b800d6265a40b96
ArviH <44539916+ArviH@users.noreply.github.com>
2020-01-08 23:33:32 -0700

    Update ColorSensor.java
    No errors

--- src/main/java/frc/robot/ColorSensor.java
+++ src/main/java/frc/robot/ColorSensor.java
@@ -14,7 +14,13 @@ public class ColorSensor {
     private static final I2C.Port i2cPort = I2C.Port.kOnboard;
     private static final ColorSensorV3 m_colorSensor = new ColorSensorV3(i2cPort);
     private static final ColorMatch m_colorMatcher = new ColorMatch();
+    private Color detectedColor = m_colorSensor.getColor();
 
+    private final Color kBlueTarget = ColorMatch.makeColor(0.143, 0.427, 0.429);
+    private final Color kGreenTarget = ColorMatch.makeColor(0.197, 0.561, 0.240);
+    private final Color kRedTarget = ColorMatch.makeColor(0.561, 0.232, 0.114);
+    private final Color kYellowTarget = ColorMatch.makeColor(0.361, 0.524, 0.113);
+    
     private ColorSensor(){}
 
     public static ColorSensor getInstance(){
@@ -23,13 +29,6 @@ public class ColorSensor {
         }
         return instance;
     }
-   
-    private final Color kBlueTarget = ColorMatch.makeColor(0.143, 0.427, 0.429);
-    private final Color kGreenTarget = ColorMatch.makeColor(0.197, 0.561, 0.240);
-    private final Color kRedTarget = ColorMatch.makeColor(0.561, 0.232, 0.114);
-    private final Color kYellowTarget = ColorMatch.makeColor(0.361, 0.524, 0.113);
-    Color detectedColor = m_colorSensor.getColor();
-
 
     public  void setColor(){
       m_colorMatcher.addColorMatch(kBlueTarget);
@@ -54,11 +53,11 @@ public class ColorSensor {
         colorString = "Unknown";
       }
      
-      SmartDashboard.putNumber("Red", detectedColor.red);
-      SmartDashboard.putNumber("Green", detectedColor.green);
-      SmartDashboard.putNumber("Blue", detectedColor.blue);
-      SmartDashboard.putNumber("Confidence", match.confidence);
-      SmartDashboard.putString("Detected Color", colorString);
+     // SmartDashboard.putNumber("Red", detectedColor.red);
+      // SmartDashboard.putNumber("Green", detectedColor.green);
+      // SmartDashboard.putNumber("Blue", detectedColor.blue);
+      // SmartDashboard.putNumber("Confidence", match.confidence);
+      // SmartDashboard.putString("Detected Color", colorString);
     }
 
 
