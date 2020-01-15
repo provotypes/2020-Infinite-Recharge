70ecb1b796c910b5d062a08575c6673814387b34
Dallin <rasmudal12@gmail.com>
2020-01-14 21:00:41 -0700

    Worked on auto(default framework)

--- src/main/java/frc/robot/autotasks/AutoChooser.java
+++ src/main/java/frc/robot/autotasks/AutoChooser.java
@@ -4,12 +4,15 @@ import java.util.ArrayList;
 
 public class AutoChooser {
 
-    ArrayList<TaskInterface> taskList = new ArrayList<TaskInterface>();
-
-	AutoFactory autoFactory;
-
-	public AutoChooser(AutoFactory factory) {
-		autoFactory = factory;
+	private static AutoFactory autoFactory;
+	public void setAutoFactory(AutoFactory factory1) {
+		autoFactory = factory1;
+	}
+	public AutoRoutine getChosenAuto() {
+		String autoSelected = AutoSetup.getAutoChooser().getSelected();
+		System.out.println("Auto selected" + autoSelected);
+		AutoRoutine chosenRoutine = new AutoRoutine(autoFactory.emptyList());
+		return chosenRoutine;
 	}
 
 }
--- src/main/java/frc/robot/autotasks/AutoFactory.java
+++ src/main/java/frc/robot/autotasks/AutoFactory.java
@@ -1,4 +1,17 @@
 package frc.robot.autotasks;
 
+import java.util.ArrayList;
+import java.util.List;
+
+
+
 public class AutoFactory {
+
+    public static List<TaskInterface> emptyList() {
+        return new ArrayList<TaskInterface>();
+
+    }
+
+
+
 }
