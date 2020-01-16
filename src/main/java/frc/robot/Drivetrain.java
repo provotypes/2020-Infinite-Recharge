1bd4f35cccd3f643643295c669d37a17e0d1a2c2
ArviH <44539916+ArviH@users.noreply.github.com>
2020-01-15 23:27:17 -0700

    Started working on anti tipping mechanism

--- src/main/java/frc/robot/Drivetrain.java
+++ src/main/java/frc/robot/Drivetrain.java
@@ -1,7 +1,8 @@
 package frc.robot;
 
-//  import com.analog.adis16470.frc.ADIS16470_IMU;
+import com.analog.adis16470.frc.ADIS16470_IMU;
 
+import edu.wpi.first.wpilibj.AnalogGyro;
 import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.SpeedControllerGroup;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
@@ -14,8 +15,11 @@ public class Drivetrain extends DifferentialDrive implements EasyPathDrivetrain{
     private static SpeedControllerGroup rightGroup;
     private static Drivetrain instance;
     private static int DISTANCE_PER_PULSE = 1;
-    // private static ADIS16470_IMU IMU = new ADIS16470_IMU();
-     
+    private static ADIS16470_IMU IMU = new ADIS16470_IMU();
+    private static AnalogGyro gyro = new AnalogGyro(1);
+    private static double targetPosition = 0.0;
+    private static double kP; //figure out later
+    
     private Drivetrain() {
         super(leftGroup, rightGroup);
         leftEncoder.setDistancePerPulse(DISTANCE_PER_PULSE);
@@ -78,8 +82,19 @@ public class Drivetrain extends DifferentialDrive implements EasyPathDrivetrain{
     public void safeArcade() {
 
     }
+// also, target position is not necessary since it is always 0 
 
+// continue working on this when its not 12am
+///////////////////////////No ONE MESS With this code///////////////////////////////////////////////
     public static void update() {
-        
+       // if(IMU.getAngle() <= whatever angle threshhold that will tip us) {
+        double turningValue = (targetPosition - IMU.getAngle()) * kP;
+       //Speed Controller Groups are confusing right now as we don't have a good design of the robot.
+        leftGroup.set(turningValue);
+        rightGroup.set(turningValue);
+     //   } else if(IMU.getAngle() >= whatever value that will tip us backwards) {
+//              repeat process
+     //}
+
     }
 }
