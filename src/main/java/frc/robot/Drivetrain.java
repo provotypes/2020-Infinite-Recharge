7c8856de2b11d8bc15bbe5f59af3b4a85eb9439b
ArviH <44539916+ArviH@users.noreply.github.com>
2020-01-16 19:17:33 -0700

    Worked on Anti Tipping Mechanism

--- src/main/java/frc/robot/Drivetrain.java
+++ src/main/java/frc/robot/Drivetrain.java
@@ -2,7 +2,6 @@ package frc.robot;
 
 import com.analog.adis16470.frc.ADIS16470_IMU;
 
-import edu.wpi.first.wpilibj.AnalogGyro;
 import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.SpeedControllerGroup;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
@@ -16,8 +15,6 @@ public class Drivetrain extends DifferentialDrive implements EasyPathDrivetrain{
     private static Drivetrain instance;
     private static int DISTANCE_PER_PULSE = 1;
     private static ADIS16470_IMU IMU = new ADIS16470_IMU();
-    private static AnalogGyro gyro = new AnalogGyro(1);
-    private static double targetPosition = 0.0;
     private static double kP; //figure out later
     
     private Drivetrain() {
@@ -46,15 +43,14 @@ public class Drivetrain extends DifferentialDrive implements EasyPathDrivetrain{
 
     @Override
     public double getCurrentAngle() {
-        // return IMU.getAngle();
-        return 0;
+        return IMU.getAngle();
     }
 
     @Override
     public void resetEncodersAndGyro() {
-       leftEncoder.reset();
-       rightEncoder.reset();
-    //    IMU.reset();
+        leftEncoder.reset();
+        rightEncoder.reset();
+        IMU.reset();
     }
 
 	public void setBrake() {
@@ -76,25 +72,16 @@ public class Drivetrain extends DifferentialDrive implements EasyPathDrivetrain{
 
 
 	public void calibrateGyro() {
-        // IMU.calibrate();
+        IMU.calibrate();
     }
 
     public void safeArcade() {
 
     }
-// also, target position is not necessary since it is always 0 
 
-// continue working on this when its not 12am
-///////////////////////////No ONE MESS With this code///////////////////////////////////////////////
-    public static void update() {
-       // if(IMU.getAngle() <= whatever angle threshhold that will tip us) {
-        double turningValue = (targetPosition - IMU.getAngle()) * kP;
-       //Speed Controller Groups are confusing right now as we don't have a good design of the robot.
-        leftGroup.set(turningValue);
-        rightGroup.set(turningValue);
-     //   } else if(IMU.getAngle() >= whatever value that will tip us backwards) {
-//              repeat process
-     //}
-
+    public void antiTippingMechanism() {
+        double turningValue = (IMU.getAngle()) * kP;
+        tankDrive(turningValue, turningValue);
     }
+
 }
