ec8069ba4de73b5f080c8e87586c9b421698f2e0
ArviH <44539916+ArviH@users.noreply.github.com>
2020-01-16 22:43:11 -0700

    Update TeleopController.java

--- src/main/java/frc/robot/TeleopController.java
+++ src/main/java/frc/robot/TeleopController.java
@@ -1,5 +1,26 @@
 package frc.robot;
 
 public class TeleopController {
+
+    private static TeleopController instance;
+
+    private TeleopController() {}
+
+    public static TeleopController getInstance() {
+        if(instance == null) {
+            instance = new TeleopController();
+        }
+        return instance;
+    }
+
+
+
+
+
+
+
+
+
+
     
 }
