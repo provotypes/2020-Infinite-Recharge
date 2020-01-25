26c0dbd5813e089cd2637062fab87ca3fde22c69
ArviH <44539916+ArviH@users.noreply.github.com>
2020-01-25 15:37:21 -0700

    Update LimelightVisionTracking.java

--- src/main/java/frc/robot/LimelightVisionTracking.java
+++ src/main/java/frc/robot/LimelightVisionTracking.java
@@ -1,13 +1,21 @@
 package frc.robot;
 
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.networktables.NetworkTableInstance;
 
 public class LimelightVisionTracking {
 
-private static LimelightVisionTracking instance;
+    private static LimelightVisionTracking instance;
+    private NetworkTable table = NetworkTableInstance.getDefault().getTable("limelight");
+    private NetworkTableEntry tx = table.getEntry("tx");
+    private NetworkTableEntry ty = table.getEntry("ty");
+    private NetworkTableEntry tv = table.getEntry("tv");
+
+    //fix me
+    private final double REL_TARGET_HEIGHT = 100; // this should definitly be changed || the target height - shooter height. 
+    //fix me
+    private final double MOUNT_ANGLE = 45; // this should be the mount angle for the limelight + the limelight angle
 
     private LimelightVisionTracking() {}
 
@@ -18,5 +26,18 @@ private static LimelightVisionTracking instance;
         return instance;
     }
 
+    public double getHorizontalAngle(){
+        double horizontalAngle = tx.getDouble(0.0);
+        return horizontalAngle;
+    } 
+
+    public double getDistance(){
+        double distance = REL_TARGET_HEIGHT/Math.tan(MOUNT_ANGLE + ty.getDouble(0.0));
+        return distance;
+    }
+
+    public boolean isthereatarget(){
+        return tv.getBoolean(true);
+    }
 
 }
