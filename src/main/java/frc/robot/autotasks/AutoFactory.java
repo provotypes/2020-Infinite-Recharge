700ae01cc78d84a1f8a5ac2839f95d007e1f4ab0
cabobalot <casey8cockrell@gmail.com>
2020-03-10 22:14:23 -0600

    add an easypath auto

--- src/main/java/frc/robot/autotasks/AutoFactory.java
+++ src/main/java/frc/robot/autotasks/AutoFactory.java
@@ -3,8 +3,7 @@ package frc.robot.autotasks;
 import java.util.ArrayList;
 import java.util.List;
 
-import frc.robot.easypath.FollowPath;
-import frc.robot.easypath.PathUtil;
+import frc.robot.easypath.*;
 
 public class AutoFactory {
 
@@ -48,16 +47,16 @@ public class AutoFactory {
     }
 
     public static List<TaskInterface> MIDDLE_AUTO() {
-		List<TaskInterface> taskList = new ArrayList<TaskInterface>();
-        taskList.add(new ShootingMechanismTask(3));
+        List<TaskInterface> taskList = new ArrayList<TaskInterface>();
         taskList.add(new IntakeMechanismTask());
-        // taskList.add(new EasyPathTask(new FollowPath(new Path(t ->
-        /*{"start":{"x":94,"y":208},"mid1":{"x":179,"y":214},"mid2":{"x":267,"y":300},"end":{"x":260,"y":229}} */
-        // (-711 * Math.pow(t, 2) + 480 * t + 18) / (-294 * Math.pow(t, 2) + 18 * t + 255),
-        // 196.337),
-        // -0.7)));
+        taskList.add(new ShootingMechanismTask(3));
+        taskList.add(new EasyPathTask(new FollowPath( new Path(t -> 
+		/* {"start":{"x":0,"y":100},"mid1":{"x":79,"y":101},"mid2":{"x":8,"y":41},"end":{"x":156,"y":40}} */
+		(360 * Math.pow(t, 2) + -366 * t + 3) / (1107 * Math.pow(t, 2) + -900 * t + 237),
+		176.61),
+        -0.5)));
         // Shoot 2 or a very large number?
-        taskList.add(new ShootingMechanismTask(2));
+        taskList.add(new ShootingMechanismTask(3));
         return taskList;
     }
 
